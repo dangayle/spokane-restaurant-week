684290ac5da0d8de9e2fbe1fb8227e3f3bb3bd99
Dan Gayle <dangayle@gmail.com>
2013-01-26 14:08:47 -0800

    update

--- wsgi/hello.py
+++ wsgi/hello.py
@@ -7,7 +7,40 @@ import pymongo
 import datetime
 from random import choice
 from string import ascii_lowercase
-from restaurants import restaurants
+
+restaurants = ["Anthony's at Spokane Falls",
+"Bistango Martini Lounge",
+"Casper Fry",
+"Central Food",
+"Churchill's Steakhouse",
+"Ciao Mambo",
+"Clinkerdagger",
+"Hills' Restaurant and Lounge",
+"Hugos On The Hill",
+"Italia Trattoria",
+"Laguna Cafe",
+"Luigi's Italian Restaurant",
+"MacKenzie River Pizza, Grill & Pub (North)",
+"MacKenzie River Pizza, Grill & Pub (South)",
+"Maggie's South Hill Grill",
+"MAX at mirabeau",
+"Mustard Seed",
+"O'Doherty's Irish Grille",
+"Post Street Ale House",
+"Remington's @Ramada Spokane Airport",
+"Ripples Riverside Grill",
+"Rock City Grill",
+"Saranac Public House",
+"Scratch Restaurant & Rain Lounge",
+"Spencer's For Steaks And Chops",
+"Stacks at Steam Plant",
+"Steelhead Bar & Grille",
+"Thai Bamboo Restaurants",
+"The Palm Court Grill",
+"The Q",
+"The Safari Room Fresh Grill and Bar",
+"Windows of the Seasons"]
+
 
 
 bottle.debug(True)
