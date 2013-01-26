0f6b9dd21f2ac5b7eecd1b840fbc5778374bec0f
Dan Gayle <dangayle@gmail.com>
2013-01-26 14:53:59 -0800

    update

--- wsgi/hello.py
+++ wsgi/hello.py
@@ -92,7 +92,7 @@ def list_restaurants():
     collection = mongo_db.restaurants
     cursor = collection.find()
     for r in cursor:
-        print '<a href="/{r[permalink]}">{r[name]}</a>\n'.format(r)
+        print '<a href="/{0}">{1}</a>\n'.format(r['permalink'], r['name'])
 
 
 application = bottle.default_app()
