aa344ba159989c0392abeace5ca924f2276e4695
Dan Gayle <dangayle@gmail.com>
2013-01-26 14:00:04 -0800

    update

--- wsgi/hello.py
+++ wsgi/hello.py
@@ -4,6 +4,10 @@
 import os
 import bottle
 import pymongo
+import datetime
+from random import choice
+from string import ascii_lowercase
+from restaurants import restaurants
 
 
 bottle.debug(True)
@@ -19,10 +23,26 @@ mongo_db.authenticate(os.environ['OPENSHIFT_MONGODB_DB_USERNAME'],
 @bottle.route('/')
 def index():
     collection = mongo_db.test
-
     item = collection.find_one()
-
     return "hello %s, you doob" % item['name']
+
+
+# @bottle.route('/get_code/:name')
+# def get_code():
+#     collection = mongo_db.restaurants
+#     collection.find('name')
+#     ''.join(choice(ascii_lowercase) for x in range(6))
+
+
+@bottle.route('/insert_restaurants')
+def insert_restaurants():
+    collection = mongo_db.restaurants
+    for restaurant in restaurants:
+        data = {
+            "name": restaurant,
+            "codes": []
+            }
+        collection.insert(data, upsert=True)
 
 
 application = bottle.default_app()
