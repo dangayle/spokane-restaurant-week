32d7c027773a16635a99bea3d0951d47e8bd4a60
Dan Gayle <dangayle@gmail.com>
2013-01-26 16:20:19 -0800

    update

--- wsgi/hello.py
+++ wsgi/hello.py
@@ -9,39 +9,6 @@ import datetime
 from random import choice
 from string import ascii_lowercase
 
-restaurants = ["Anthony's at Spokane Falls",
-"Bistango Martini Lounge",
-"Casper Fry",
-"Central Food",
-"Churchill's Steakhouse",
-"Ciao Mambo",
-"Clinkerdagger",
-"Hills' Restaurant and Lounge",
-"Hugos On The Hill",
-"Italia Trattoria",
-"Laguna Cafe",
-"Luigi's Italian Restaurant",
-"MacKenzie River Pizza, Grill & Pub (North)",
-"MacKenzie River Pizza, Grill & Pub (South)",
-"Maggie's South Hill Grill",
-"MAX at mirabeau",
-"Mustard Seed",
-"O'Doherty's Irish Grille",
-"Post Street Ale House",
-"Remington's @Ramada Spokane Airport",
-"Ripples Riverside Grill",
-"Rock City Grill",
-"Saranac Public House",
-"Scratch Restaurant & Rain Lounge",
-"Spencer's For Steaks And Chops",
-"Stacks at Steam Plant",
-"Steelhead Bar & Grille",
-"Thai Bamboo Restaurants",
-"The Palm Court Grill",
-"The Q",
-"The Safari Room Fresh Grill and Bar",
-"Windows of the Seasons"]
-
 
 
 bottle.debug(True)
@@ -52,7 +19,8 @@ mongo_con = pymongo.Connection(os.environ['OPENSHIFT_MONGODB_DB_HOST'],
 mongo_db = mongo_con[os.environ['OPENSHIFT_APP_NAME']]
 mongo_db.authenticate(os.environ['OPENSHIFT_MONGODB_DB_USERNAME'],
                             os.environ['OPENSHIFT_MONGODB_DB_PASSWORD'])
-
+bottle.TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'],
+                                                'wsgi', 'views'))
 
 @bottle.route('/')
 def index():
