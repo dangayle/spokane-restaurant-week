c1f8634fcf38eb8dccbe3c876c075cb32aebc044
Dan Gayle <dangayle@gmail.com>
2013-01-26 22:02:58 -0800

    twilio stuff

--- wsgi/hello.py
+++ wsgi/hello.py
@@ -94,7 +94,8 @@ def get_code(permalink):
     return bottle.template('index', body=body, restaurants=restaurants)
 
 
-@bottle.post('/sms/<code>')
+@bottle.route('/sms/<code>/')
+@bottle.route('/sms/<code>')
 def get_sms(code=0):
     resp = twilio.twiml.Response()
     resp.sms("Hello, Mobile Monkey")
